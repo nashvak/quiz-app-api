482f0a4a1f90d72c57953e702f2d842642cd3fee
nashva <nashvakizhakkayil@gmail.com>
2023-12-06 15:12:41 +0530

    model class cretade

--- lib/model/quiz_model.dart
+++ lib/model/quiz_model.dart
@@ -1,4 +1,16 @@
-class QuestionsModel {
+//To parse this JSON data, do
+//
+//     final questions = questionsFromJson(jsonString);
+
+import 'dart:convert';
+
+List<Questions> questionsFromJson(String str) =>
+    List<Questions>.from(json.decode(str).map((x) => Questions.fromJson(x)));
+
+String questionsToJson(List<Questions> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Questions {
   String id;
   String question;
   List<Option> options;
@@ -6,7 +18,7 @@ class QuestionsModel {
   DateTime updatedAt;
   int v;
 
-  QuestionsModel({
+  Questions({
     required this.id,
     required this.question,
     required this.options,
@@ -14,6 +26,25 @@ class QuestionsModel {
     required this.updatedAt,
     required this.v,
   });
+
+  factory Questions.fromJson(Map<String, dynamic> json) => Questions(
+        id: json["_id"],
+        question: json["question"],
+        options:
+            List<Option>.from(json["options"].map((x) => Option.fromJson(x))),
+        createdAt: DateTime.parse(json["createdAt"]),
+        updatedAt: DateTime.parse(json["updatedAt"]),
+        v: json["__v"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "_id": id,
+        "question": question,
+        "options": List<dynamic>.from(options.map((x) => x.toJson())),
+        "createdAt": createdAt.toIso8601String(),
+        "updatedAt": updatedAt.toIso8601String(),
+        "__v": v,
+      };
 }
 
 class Option {
@@ -26,4 +57,16 @@ class Option {
     required this.isCorrect,
     required this.id,
   });
+
+  factory Option.fromJson(Map<String, dynamic> json) => Option(
+        text: json["text"],
+        isCorrect: json["isCorrect"],
+        id: json["_id"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "text": text,
+        "isCorrect": isCorrect,
+        "_id": id,
+      };
 }
--- lib/viewmodel/api_calls.dart
+++ lib/viewmodel/api_calls.dart
@@ -1,1 +1,15 @@
+import 'dart:convert';
 
+import 'package:http/http.dart' as http;
+import 'package:quiz_app_api/model/quiz_model.dart';
+
+class ApiCalls {
+  static getData() async {
+    var response = await http
+        .get(Uri.parse("https://nice-lime-hippo-wear.cyclic.app/api/v1/quiz"));
+    final decoded = json.decode(response.body) as List;
+    print(decoded);
+
+    return decoded.map((e) => Questions.fromJson(e)).toList();
+  }
+}
